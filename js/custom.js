b294527c981b76c6abd347dabd1a00be5095df6d
yishanhsieh <47290910+yishanhsieh@users.noreply.github.com>
2019-03-25 23:51:29 +0800

    animatedmodal

--- js/custom.js
+++ js/custom.js
@@ -104,7 +104,10 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+    $("#demo01").animatedModal();
+    $("#demo02").animatedModal({ modalTarget: 'animatedModal2'});
+
+
 
     // Contact Form 	
 
